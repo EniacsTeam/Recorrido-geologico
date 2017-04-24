27a845ad75a92ddde3f3b78cf680348784d2161a
fran596 <fran.arroyo596@gmail.com>
2017-04-24 11:02:15 -0600

    Agregada ventana de informacion de marcador

--- app/src/main/java/com/eniacs_team/rutamurcielago/Mapa.java
+++ app/src/main/java/com/eniacs_team/rutamurcielago/Mapa.java
@@ -1,8 +1,23 @@
 package com.eniacs_team.rutamurcielago;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.Application;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.os.Bundle;
 import android.os.Environment;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.osmdroid.events.MapListener;
@@ -18,25 +33,36 @@ import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.infowindow.InfoWindow;
 
 import java.io.File;
 import java.util.Set;
+
+import static android.R.string.no;
+import static android.R.string.yes;
 
 /**
  * Created by Johan Duran Cerdas on 19/4/2017.
  */
 
-public class Mapa {
+public class Mapa extends Application {
     MapView mapView;
+    Context mContext;
+    Activity mActivity;
+    //Dialog dialogo;
+    AlertDialog dialog;
+    CustomDialogClass dialogo;
     public static final GeoPoint pacific = new GeoPoint(11.028670, -85.704637);
 
-    public Mapa(MapView map){
+    public Mapa(MapView map, Activity activity) {
         this.mapView = map;
+        this.mActivity = activity;
     }
 
-    public void setupMap(Context context){
+    public void setupMap(Context context) {
         /*En caso de error muestra este layout*/
         mapView.getTileProvider().setTileLoadFailureImage(context.getResources().getDrawable(R.drawable.notfound));
+        this.mContext = context;
 
         /*Elementos correspondietes a funcionalidades*/
         mapView.setClickable(true);
@@ -61,11 +87,11 @@ public class Mapa {
         /*Marcador de ejemplo. Crear metodo que haga esto.*/
         Marker startMarker = new Marker(mapView);
         startMarker.setPosition(new GeoPoint(11.028670, -85.704637));
-        Drawable marker=context.getResources().getDrawable(R.mipmap.marker);
+        Drawable marker = context.getResources().getDrawable(R.mipmap.marker);
         startMarker.setIcon(marker);
         startMarker.setAnchor(Marker.ANCHOR_CENTER, 1.0f);
-        /*InfoWindow infoWindow = new MyInfoWindow(R.layout.bonuspack_bubble, mapView);
-        startMarker.setInfoWindow(infoWindow);*/
+        InfoWindow infoWindow = new MyInfoWindow(R.layout.bonuspack_bubble, mapView);
+        startMarker.setInfoWindow(infoWindow);
         startMarker.setTitle("Title of the marker");
         mapView.getOverlays().add(startMarker);
 
@@ -86,8 +112,10 @@ public class Mapa {
                 return true;
             }
         });
-    }
 
+        /*Creo el dialogo que se despliega en ver mas si no estoy cerca del punto*/
+        dialogo = new CustomDialogClass(mActivity);
+    }
 
 
     /*Define el bounding box se pueden definir distintos para cada zoom, lat,lon, lat,lon*/
@@ -96,75 +124,127 @@ public class Mapa {
     }
 
 
-
-
-
-
-public void findFiles(Context context){
+    public void findFiles(Context context) {
     /*Se busca el archivo dentro del path*/
-    File tiles = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/osmdroid/tiles.zip");
-    if (tiles.exists()) {
-                    try {
-                        //Se crea y utiliza en archivo
-                        File[] file=new File[]{tiles};
-                        OfflineTileProvider tileProvider = new OfflineTileProvider(new SimpleRegisterReceiver(context),file);
+        File tiles = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/osmdroid/tiles.zip");
+        if (tiles.exists()) {
+            try {
+                //Se crea y utiliza en archivo
+                File[] file = new File[]{tiles};
+                OfflineTileProvider tileProvider = new OfflineTileProvider(new SimpleRegisterReceiver(context), file);
 
-                        //Se asigna el proveedor de tiles
-                        mapView.setTileProvider(tileProvider);
+                //Se asigna el proveedor de tiles
+                mapView.setTileProvider(tileProvider);
 
                         /*Se obtienen los archivos dentro de la carpeta*/
-                        String source = "";
-                        IArchiveFile[] archives = tileProvider.getArchives();
+                String source = "";
+                IArchiveFile[] archives = tileProvider.getArchives();
                         /*En caso de no tener se utiliza proveedor por internet*/
-                        if (archives.length > 0) {
-                            Set<String> tileSources = archives[0].getTileSources();
-                            if (!tileSources.isEmpty()) {
-                                source = tileSources.iterator().next();
-                                mapView.setTileSource(FileBasedTileSource.getSource(source));
-                            } else {/*En caso de no existir archivo utiliza uno proporcionado por internet*/
-                                mapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
-                            }
-
-                        } else {
-                            mapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
-                        }
-
-                        mapView.invalidate();
-                        return;
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
+                if (archives.length > 0) {
+                    Set<String> tileSources = archives[0].getTileSources();
+                    if (!tileSources.isEmpty()) {
+                        source = tileSources.iterator().next();
+                        mapView.setTileSource(FileBasedTileSource.getSource(source));
+                    } else {/*En caso de no existir archivo utiliza uno proporcionado por internet*/
+                        mapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
                     }
-                    /*En los proximos dos casos podemos volver a llamar cargar assets porque es posible que la persona lo borro.*/
-        Toast.makeText(context, tiles.getAbsolutePath() + " No existe mapa, en el dispositivo, cargando desde internet", Toast.LENGTH_SHORT).show();
-    } else {
-        Toast.makeText(context, tiles.getAbsolutePath() + "El directorio no existe", Toast.LENGTH_SHORT).show();
-    }
-}
-}
-/*
-Clase de https://mobiledevstories.wordpress.com/2014/03/01/osmdroid-bonus-pack-markers-with-clickable-infowindows/
-Sirve para crear un infowindow
-private class MyInfoWindow extends InfoWindow {
-    public MyInfoWindow(int layoutResId, MapView mapView) {
-        super(layoutResId, mapView);
-    }
-    public void onClose() {
-    }
 
-    public void onOpen(Object arg0) {
-        LinearLayout layout = (LinearLayout) mView.findViewById(R.id.bubble_layout);
-        Button btnMoreInfo = (Button) mView.findViewById(R.id.bubble_moreinfo);
-        TextView txtTitle = (TextView) mView.findViewById(R.id.bubble_title);
-        TextView txtDescription = (TextView) mView.findViewById(R.id.bubble_description);
-        TextView txtSubdescription = (TextView) mView.findViewById(R.id.bubble_subdescription);
+                } else {
+                    mapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
+                }
 
-        txtTitle.setText("Title of my marker");
-        txtDescription.setText("Click here to view details!");
-        txtSubdescription.setText("You can also edit the subdescription");
-        layout.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Override Marker's onClick behaviour here
+                mapView.invalidate();
+                return;
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
-        });
+                    /*En los proximos dos casos podemos volver a llamar cargar assets porque es posible que la persona lo borro.*/
+            Toast.makeText(context, tiles.getAbsolutePath() + " No existe mapa, en el dispositivo, cargando desde internet", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(context, tiles.getAbsolutePath() + "El directorio no existe", Toast.LENGTH_SHORT).show();
+        }
     }
-}*/
+//}
+
+    //Clase de https://mobiledevstories.wordpress.com/2014/03/01/osmdroid-bonus-pack-markers-with-clickable-infowindows/
+//Sirve para crear un infowindow
+    private class MyInfoWindow extends InfoWindow {
+        public MyInfoWindow(int layoutResId, MapView mapView) {
+            super(layoutResId, mapView);
+        }
+
+        public void onClose() {
+        }
+
+        public void onOpen(Object arg0) {
+            LinearLayout layout = (LinearLayout) mView.findViewById(R.id.bonuspack_bubble);
+
+            ImageView img = (ImageView) mView.findViewById(R.id.bubble_image);
+
+            img.setImageResource(R.drawable.pt1);
+
+            TextView txtTitle = (TextView) mView.findViewById(R.id.bubble_title);
+            TextView txtDescription = (TextView) mView.findViewById(R.id.bubble_description);
+            TextView txtVerMas = (TextView) mView.findViewById(R.id.ver_mas);
+            View viewLinea = (View) mView.findViewById(R.id.linea_centro);
+            viewLinea.setMinimumWidth(txtDescription.getWidth());
+
+
+
+            txtVerMas.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    dialogo.show();
+                   // dialog.show();
+                }
+
+
+            });
+            txtTitle.setText("Punto #1");
+            txtDescription.setText("Esta formación se compone de intercalaciones de areniscas y lutitas de edad entre 60 y 40 millones de años");
+
+            layout.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    // Override Marker's onClick behaviour here
+                }
+            });
+        }
+
+
+    }
+
+    public class CustomDialogClass extends Dialog implements
+            android.view.View.OnClickListener {
+
+        public Activity c;
+        public Button btnAceptar;
+
+        public CustomDialogClass(Activity a) {
+            super(a);
+            this.c = a;
+        }
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            setContentView(R.layout.dialogo_ver_mas);
+            btnAceptar = (Button) findViewById(R.id.aceptar);
+            btnAceptar.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.aceptar:
+                    dismiss();
+                    break;
+                default:
+                    break;
+            }
+            dismiss();
+        }
+    }
+}
