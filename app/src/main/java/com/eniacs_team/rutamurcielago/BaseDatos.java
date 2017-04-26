beccae2c752276e16622f9798489e7b6cc49db0b
david.chaves <david.chaves@me.com>
2017-04-26 15:23:47 -0600

    Integración de consultas con el master

--- app/src/main/java/com/eniacs_team/rutamurcielago/BaseDatos.java
+++ app/src/main/java/com/eniacs_team/rutamurcielago/BaseDatos.java
@@ -1,14 +1,11 @@
 package com.eniacs_team.rutamurcielago;
 
-import android.app.ActivityManager;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
-
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
