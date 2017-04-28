c1a72fc1a564b9606226ade3e3da98092f538b93
Red-Skull <solorzano.chelo@gmail.com>
2017-04-27 18:39:01 -0600

    Agregada animacion en realidad aumentada.

--- app/src/main/java/com/eniacs_team/rutamurcielago/CameraOSMaps.java
+++ app/src/main/java/com/eniacs_team/rutamurcielago/CameraOSMaps.java
@@ -1,12 +1,15 @@
 package com.eniacs_team.rutamurcielago;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.beyondar.android.fragment.BeyondarFragmentSupport;
 import com.beyondar.android.world.World;
@@ -18,6 +21,9 @@ public class CameraOSMaps extends FragmentActivity implements OnClickListener{
 
     private Button mShowMap;
 
+    private AnimationDrawable mouthAnimation;
+    private ImageButton charButton;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,13 @@ public class CameraOSMaps extends FragmentActivity implements OnClickListener{
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         loadViewFromXML();
+
+        ImageView mouthImage = (ImageView) findViewById(R.id.imageView);
+        mouthImage.setBackgroundResource(R.drawable.mouth_anim);
+        mouthAnimation = (AnimationDrawable) mouthImage.getBackground();
+
+        charButton = (ImageButton) findViewById(R.id.imageButton);
+        charButton.setOnClickListener(this);
 
         // We create the world and fill it
         mWorld = CustomWorldHelper.generateObjects(this);
@@ -50,6 +63,10 @@ public class CameraOSMaps extends FragmentActivity implements OnClickListener{
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         }
+        else if (v == charButton) {
+            mouthAnimation.start();
+        }
     }
+
 
 }
