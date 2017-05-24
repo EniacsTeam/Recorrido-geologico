cc5045ad9a246bfcad2c6ccf3199fcdeabc94bbe
Red-Skull <solorzano.chelo@gmail.com>
2017-05-24 17:21:54 -0600

    Agregado muestra para cliente.

--- app/src/main/java/com/eniacs_team/rutamurcielago/CustomWorldHelper.java
+++ app/src/main/java/com/eniacs_team/rutamurcielago/CustomWorldHelper.java
@@ -31,7 +31,6 @@ public class CustomWorldHelper {
      */
     private static void agregarCarteles() {
         /*Se crea un cartel para cada punto en el mapa*/
-
         for (int i = 0; i < DatosGeo.cantidadElementos; i++)
         {
             // Create an object with an image in the app resources.
@@ -42,6 +41,33 @@ public class CustomWorldHelper {
             // Add the GeoObjects to the world
             sharedWorld.addBeyondarObject(go);
         }
+
+        /* Para muestra al cliente
+         * Comentar bloque for arriba
+         * Descomentar lo siguiente */
+
+        /*
+        GeoObject go1 = new GeoObject(100);
+        go1.setGeoPosition(10.951271d, -85.709449d);
+        go1.setImageResource(R.drawable.descartes);
+        go1.setName(baseDatos.selectDescripcion(1));
+
+        // Create an object with an image in the app resources.
+        GeoObject go2 = new GeoObject(101);
+        go2.setGeoPosition(10.951394d, -85.709024d);
+        go2.setImageResource(R.drawable.vegetacion);
+        go2.setName(baseDatos.selectDescripcion(2));
+
+        // Create an object with an image in the app resources.
+        GeoObject go3 = new GeoObject(102);
+        go3.setGeoPosition(10.951771d, -85.709807d);
+        go3.setImageResource(R.drawable.peridotitas);
+        go3.setName(baseDatos.selectDescripcion(3));
+
+        sharedWorld.addBeyondarObject(go1);
+        sharedWorld.addBeyondarObject(go2);
+        sharedWorld.addBeyondarObject(go3);
+        */
     }
 
     /**
@@ -60,9 +86,9 @@ public class CustomWorldHelper {
         // User position (you can change it using the GPS listeners form Android
         // API)
         //sharedWorld.setGeoPosition(10.926201d, -85.818870d);
-        //sharedWorld.setGeoPosition(10.951398d, -85.709450d);
+        sharedWorld.setGeoPosition(10.951398d, -85.709450d); //Cerca de descartes
 
-        sharedWorld.setGeoPosition(10.9408d, -85.774d);
+        //sharedWorld.setGeoPosition(10.9408d, -85.774d); //Cerquisima de vegetacion
 
         agregarCarteles();
 
